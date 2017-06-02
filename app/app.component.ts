92cc8e9268da3b508aa21e40d108b718d0959d8e
iyke <nderiony95@gmail.com>
2017-06-02 11:04:35 +0300

    add initial files

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component}from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: `
+<div class="container">
+    <h1>Meal Tracker</h1>
+</div>  `
+})
+export class AppComponent{
+
+}
