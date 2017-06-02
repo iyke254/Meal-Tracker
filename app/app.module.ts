d7e9152f6728642f8cb19acee9ebfebb3a65dc5a
iyke <nderiony95@gmail.com>
2017-06-02 13:55:39 +0300

    added new ts files

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,27 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppComponent} from './app.component';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }   from './app.component';
+import { TaskListComponent } from './meal-list.component';
+import { EditTaskComponent } from './edit-meal.component';
+import { NewTaskComponent } from './new-meal.component';
+import { CompletenessPipe } from './completeness.pipe';
+import { TaskComponent } from './meal.component';
 
 @NgModule({
-  imports:[BrowserModule],
-  declarations:[AppComponent],
-  bootstrap:[AppComponent]
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    MealListComponent,
+    EditMealComponent,
+    NewMEalComponent,
+    CompletenessPipe,
+    MealComponent,
+  ],
+  bootstrap:    [ AppComponent ]
 })
-export class AppModule {}
+
+export class AppModule { }
